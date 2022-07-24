65472192bb4b89745bc1295f87ed1382a6d698da
TomSoerr <tomix2004@gmail.com>
2022-07-24 14:36:32 +0200

    Add caesarCipher function and tests

--- practice.js
+++ practice.js
@@ -5,18 +5,21 @@ function toStringArray(input) {
   return arr;
 }
 
+// 1
 function capitalize(input) {
   const arr = toStringArray(input);
   arr.splice(0, 1, arr[0].toUpperCase());
   return arr.join('');
 }
 
+// 2
 function reverseString(input) {
   const arr = toStringArray(input);
   arr.reverse();
   return arr.join('');
 }
 
+// 3
 const calculator = {
   add(a, b) {
     return a + b;
@@ -32,4 +35,63 @@ const calculator = {
   },
 };
 
-export { capitalize, reverseString, calculator };
+// helper function for caesarCipher
+const alphabet = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+function getShiftedLetter(letter, shift) {
+  const getIndex = (l) => {
+    let index = alphabet.indexOf(l) + shift;
+    if (index > 25) {
+      index -= 26;
+    }
+    return index;
+  };
+  if (alphabet.includes(letter)) {
+    const index = getIndex(letter);
+    return alphabet[index];
+  }
+  if (alphabet.includes(letter.toLowerCase())) {
+    const index = getIndex(letter.toLowerCase());
+    return alphabet[index].toUpperCase();
+  }
+  return letter;
+}
+
+// 4
+function caesarCipher(string, shift) {
+  if (shift > 25 || shift < 0) throw new Error('invalid shift');
+  const arr = string.split('');
+  for (let x = 0; x < arr.length; x += 1) {
+    arr.splice(x, 1, getShiftedLetter(arr[x], shift));
+  }
+  return arr.join('');
+}
+
+export { capitalize, reverseString, calculator, caesarCipher };
--- practice.test.js
+++ practice.test.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-undef */
-import { capitalize, reverseString, calculator } from './practice';
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+} from './practice';
 
 test('capitalize: Easy Path 1', () => {
   expect(capitalize('bababoi')).toMatch(/^Bababoi$/);
@@ -67,4 +72,56 @@ test('calculator multiply: 4 * 4', () => {
 
 test('calculator multiply: 100 * 0.3', () => {
   expect(calculator.multiply(100, 0.3)).toEqual(30);
+});
+
+test('caesarCipher: Easy Path 1', () => {
+  expect(caesarCipher('tom', 1)).toMatch(/^upn$/);
+});
+
+test('caesarCipher: Easy Path 2', () => {
+  expect(caesarCipher('tom', 5)).toMatch(/^ytr$/);
+});
+
+test('caesarCipher: wrapping from z to a', () => {
+  expect(caesarCipher('z', 1)).toMatch(/^a$/);
+});
+
+test('caesarCipher: wrapping from t to i', () => {
+  expect(caesarCipher('t', 15)).toMatch(/^i$/);
+});
+
+test('caesarCipher: upper case', () => {
+  expect(caesarCipher('A', 2)).toMatch(/^C$/);
+});
+
+test('caesarCipher: upper case plus wrapping', () => {
+  expect(caesarCipher('Z', 1)).toMatch(/^A$/);
+});
+
+test('caesarCipher: special character .', () => {
+  expect(caesarCipher('oh...', 2)).toMatch(/^qj...$/);
+});
+
+test('caesarCipher: special character "', () => {
+  expect(caesarCipher('"something"', 25)).toMatch(/^"rnldsghmf"$/);
+});
+
+test('caesarCipher: special character [whitespace]', () => {
+  expect(caesarCipher('my name', 10)).toMatch(/^wi xkwo$/);
+});
+
+test('caesarCipher: no shift', () => {
+  expect(caesarCipher('Hello', 0)).toMatch(/^Hello$/);
+});
+
+test('caesarCipher: invalid shift 1', () => {
+  expect(() => caesarCipher('error', 26)).toThrow('invalid shift');
+});
+
+test('caesarCipher: invalid shift 2', () => {
+  expect(() => caesarCipher('error', -1)).toThrow('invalid shift');
+});
+
+test('caesarCipher: empty', () => {
+  expect(caesarCipher('')).toMatch(/^$/);
 });
